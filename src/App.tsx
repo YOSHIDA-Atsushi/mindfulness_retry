48d7be92407022ab1c6063c346cb58e0c07a53ae
YOSHIDA Atsushi <yoshida.atsushi@outlook.com>
2020-06-21 08:48:31 +0900

    mv App.tsx and fix prettierrc.yml

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+    <>
+      <Text>hello world</Text>
+    </>
+  );
+};
+
+export default App;
