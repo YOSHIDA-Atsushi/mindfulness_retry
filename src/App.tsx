e08ccb3d96d74609b79b9107b09dc33b7a297cb4
YOSHIDA Atsushi <yoshida.atsushi@outlook.com>
2020-06-21 08:54:12 +0900

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-declare const global: { HermesInternal: null | {} };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-const App = () => {
+export default function App() {
   return (
-    <>
+    <View style={styles.container}>
       <Text>hello world</Text>
-    </>
+    </View>
   );
-};
-
-export default App;
+}
