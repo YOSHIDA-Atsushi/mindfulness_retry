5a93879d5ed07f1ebc8bd0aeb106d47e0c23df23
YOSHIDA Atsushi <yoshida.atsushi@outlook.com>
2020-06-22 20:56:21 +0900

    added a slider. can show artwork

--- src/components/pages/Player/index.tsx
+++ src/components/pages/Player/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet /*TouchableOpacity*/ } from 'react-native';
+import { View, Text, StyleSheet, Image /*TouchableOpacity*/ } from 'react-native';
 //import SafeAreaView from 'react-native-safe-area-view';
 import { Button } from 'react-native-paper';
 import RNTrackPlayer from 'react-native-track-player';
+import { useTrackPlayerProgress } from 'react-native-track-player';
+import Slider from '@react-native-community/slider';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
     height: 100,
   },
 });
-
+/*
 //RNTrackPlayer.getState() returns 2 (stopped) or 3(playing). would like a boolean
 function stateNumToBoolean(i: number | string): boolean {
   switch (i) {
@@ -26,10 +28,15 @@ function stateNumToBoolean(i: number | string): boolean {
       return false;
   }
 }
+*/
 
 export default function Player({ route }) {
+  const { position, duration } = useTrackPlayerProgress();
   const { item } = route.params;
-  const [stateAndPosition, setStateAndPosition] = useState({ isPlaying: true, position: 0 });
+  const [isPlaying, setIsPlaying] = useState(true);
+  //  const duration = item.duration;
+  console.log(duration);
+  console.log(position);
   useEffect(() => {
     RNTrackPlayer.setupPlayer();
     RNTrackPlayer.add([item]);
@@ -38,20 +45,28 @@ export default function Player({ route }) {
   return (
     <View style={styles.container}>
       <Text>{item.title}</Text>
+      <Image source={item.artwork} style={{ width: 200, height: 200 }} />
+      <Slider
+        style={{ width: 300, height: 40 }}
+        minimumValue={0}
+        maximumValue={duration}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+        onValueChange={time => RNTrackPlayer.seekTo(time)}
+        value={position}
+      />
       <Button
-        icon={stateAndPosition.isPlaying ? 'pause' : 'play'}
-        mode="contained"
+        icon={isPlaying ? 'pause' : 'play'}
         onPress={async () => {
-          const state: number | string = await RNTrackPlayer.getState();
-          const isPlaying: boolean = stateNumToBoolean(state);
-          const position: number = await RNTrackPlayer.getPosition();
+          //const tempState: number | string = await RNTrackPlayer.getState();
+          //const tempIsPlaying: boolean = stateNumToBoolean(tempState);
           if (isPlaying) {
             RNTrackPlayer.pause();
           } else {
             RNTrackPlayer.play();
-            RNTrackPlayer.seekTo(stateAndPosition.position);
+            RNTrackPlayer.seekTo(position);
           }
-          setStateAndPosition({ isPlaying: !isPlaying, position: position });
+          setIsPlaying(!isPlaying);
         }}
       >
         {' '}
