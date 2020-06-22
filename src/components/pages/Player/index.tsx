ad8092c4895cde3a81cbd2c2d251cc9a1c0e3c1f
YOSHIDA Atsushi <yoshida.atsushi@outlook.com>
2020-06-22 18:56:23 +0900

    has two states, isPlaying and position, using useState()

--- src/components/pages/Player/index.tsx
+++ src/components/pages/Player/index.tsx
@@ -15,9 +15,21 @@ const styles = StyleSheet.create({
   },
 });
 
+//RNTrackPlayer.getState() returns 2 (stopped) or 3(playing). would like a boolean
+function stateNumToBoolean(i: number | string): boolean {
+  switch (i) {
+    case 2:
+      return false;
+    case 3:
+      return true;
+    default:
+      return false;
+  }
+}
+
 export default function Player({ route }) {
   const { item } = route.params;
-  const [position, setPosition] = useState(0);
+  const [stateAndPosition, setStateAndPosition] = useState({ isPlaying: true, position: 0 });
   useEffect(() => {
     RNTrackPlayer.setupPlayer();
     RNTrackPlayer.add([item]);
@@ -27,19 +39,19 @@ export default function Player({ route }) {
     <View style={styles.container}>
       <Text>{item.title}</Text>
       <Button
-        icon="play"
+        icon={stateAndPosition.isPlaying ? 'pause' : 'play'}
         mode="contained"
         onPress={async () => {
-          const state = await RNTrackPlayer.getState();
-          console.log(state);
-
-          if (state === 3) {
-            setPosition(await RNTrackPlayer.getPosition());
+          const state: number | string = await RNTrackPlayer.getState();
+          const isPlaying: boolean = stateNumToBoolean(state);
+          const position: number = await RNTrackPlayer.getPosition();
+          if (isPlaying) {
             RNTrackPlayer.pause();
-            return;
+          } else {
+            RNTrackPlayer.play();
+            RNTrackPlayer.seekTo(stateAndPosition.position);
           }
-          RNTrackPlayer.play();
-          RNTrackPlayer.seekTo(position);
+          setStateAndPosition({ isPlaying: !isPlaying, position: position });
         }}
       >
         {' '}
