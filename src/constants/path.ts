267e6931f50a457db52df682425b772b2d8f9663
YOSHIDA Atsushi <yoshida.atsushi@outlook.com>
2020-06-24 00:02:52 +0900

    added tab icon

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
 export const PLAYLIST = 'PlayList';
-export const QANDA = 'QandA';
+export const QANDA = 'Q＆A';
 export const SETTINGS = 'Settings';
 export const PLAYER = 'Player';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 //import SafeAreaView from 'react-native-safe-area-view';
 import { QandA, Settings, Player, PlayList } from './../components/pages';
 import { PLAYLIST, QANDA, SETTINGS, PLAYER } from './../constants/path';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //tab
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === PLAYLIST) {
+            iconName = 'ios-musical-notes';
+          } else if (route.name === SETTINGS) {
+            iconName = 'ios-settings';
+          } else if (route.name === QANDA) {
+            iconName = 'md-help';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name={PLAYLIST} component={PlayList} />
       <Tab.Screen name={QANDA} component={QandA} />
       <Tab.Screen name={SETTINGS} component={Settings} />
